03c8b68977484d51cac2ca0987a6d684580333ec
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2022-02-18 17:28:22 +0100

    ch tidy data finished

--- 01_getting_started/12_tidy_data.R
+++ 01_getting_started/12_tidy_data.R
@@ -17,10 +17,15 @@ library(tidyverse)
 #     across(population, as.integer)
 #   )
 
+### Tidy Data
+
+table1
+
 # compute rate per 10,000
 table1 %>% 
-  mutate(rate=cases/population * 10000)
+  mutate(rate = cases / population * 10000)
 
+# count cases for each year
 table1 %>% 
   count(year, wt = cases)
 
@@ -73,7 +78,7 @@ df
 # Which representation is easiest to work with? Which is hardest? Why?
 
 
-# Pivot
+### Pivoting
 
 # Problem: one variable is spread across multiple columns
 # Result: column names are values, not variables
@@ -95,11 +100,217 @@ table4b
 tidy4b <- table4b %>%
   pivot_longer(
     cols = c(`1999`, `2000`),
-    names_to = "year",
-    values_to = "population"
+    names_to = "year",            # must be quoted
+    values_to = "population"      # since these columns do not yet exist
   )
 tidy4b
 
 # put "cases" and "population" together
 left_join(tidy4a, tidy4b, by = c("country", "year"))
 
+# Problem: one variable is spread across multiple rows
+# Result: variables are values in cells
+# here: "cases" and "population" are values in column "type",
+#       but should be columns
+table2
+# tidy by separating these variables
+table2 %>% 
+  pivot_wider(
+    names_from = type,            # no quotes needed
+    values_from = count           # since already a column
+  )
+
+# Exercises
+# 1. Why are pivot_longer() and pivot_wider() not perfectly symmetrical?
+#    Carefully consider the following example:
+stocks <- tibble(
+  year   = c(2015, 2015, 2016, 2016),
+  half  = c(    1,    2,    1,    2),
+  return = c(1.88, 0.59, 0.92, 0.17)
+)
+stocks
+
+stocks %>% 
+  pivot_wider(names_from = year, values_from = return) %>% 
+  pivot_longer(`2015`:`2016`, names_to = "year", values_to = "return")
+
+stocks %>%
+  pivot_wider(
+    names_from = year,
+    values_from = return) %>%
+  pivot_longer(
+    cols = c(`2015`, `2016`),
+    names_to = "year",
+    values_to = "return",
+    names_transform = list(year = as.numeric) # fix wrong data type
+  )
+
+# 2. Why does this code fail?
+table4a %>%
+  pivot_longer(cols = c(1999, 2000),
+               names_to = "year",
+               values_to = "cases")
+# column names have to be:
+#   - non-syntactic c(`1999`, `2000`) or
+#   - characters c("1999", "2000")
+
+# 3. What would happen if you widen this table? Why?
+#    How could you add a new column to uniquely identify each value?
+people <- tribble(
+  ~name,             ~names,  ~values,
+  #-----------------|--------|------
+  "Phillip Woods",   "age",       45,
+  "Phillip Woods",   "height",   186,
+  "Phillip Woods",   "age",       50,
+  "Jessica Cordero", "age",       37,
+  "Jessica Cordero", "height",   156
+)
+people %>%pivot_wider(names_from = "names",
+                      values_from = "values")
+
+
+# 4. Tidy the simple tibble below.
+#    Do you need to make it wider or longer? What are the variables?
+preg <- tribble(
+  ~pregnant, ~male, ~female,
+  "yes",     NA,    10,
+  "no",      20,    12
+)
+preg
+preg %>%
+  pivot_longer(
+    cols = c("male", "female"),
+    names_to = "sex",
+    values_to = "count",
+    values_drop_na = TRUE
+  )
+
+
+### Separating and Uniting
+
+table3
+
+# separate "cases" and "population" from "rate"
+table3 %>% 
+  separate(col = "rate", into = c("cases", "population"))
+# with type conversion (make "cases" and "population" integers)
+table3 %>% 
+  separate(col = "rate", into = c("cases", "population"), convert = TRUE)
+
+table5
+# unite "century" and "year"
+table5 %>% 
+  unite(col = "new", c("century", "year"), sep = "") %>% 
+  mutate(across(new, as.integer))
+
+# Exercises
+# 1. What do the extra and fill arguments do in separate()?
+#    Experiment with the various options for the following two toy datasets.
+tibble(x = c("a,b,c", "d,e,f,g", "h,i,j")) %>% 
+  separate(x, c("one", "two", "three"))
+# merge extra values in last column
+tibble(x = c("a,b,c", "d,e,f,g", "h,i,j")) %>% 
+  separate(x, c("one", "two", "three"), extra = "merge")
+
+tibble(x = c("a,b,c", "d,e", "f,g,i")) %>% 
+  separate(x, c("one", "two", "three"))
+# fill missing values on the right
+tibble(x = c("a,b,c", "d,e", "f,g,i")) %>% 
+  separate(x, c("one", "two", "three"), fill="right", remove = FALSE)
+# 2. Both unite() and separate() have a remove argument. What does it do?
+#    Why would you set it to FALSE?
+
+#    To compare with the original column
+tibble(x = c("a,b,c", "d,e", "f,g,i")) %>% 
+  separate(
+    col = x,
+    into = c("one", "two", "three"),
+    fill="right",
+    remove = FALSE)
+
+### Missing Values
+
+stocks <- tibble(
+  year   = c(2015, 2015, 2015, 2015, 2016, 2016, 2016),
+  qtr    = c(   1,    2,    3,    4,    2,    3,    4),
+  return = c(1.88, 0.59, 0.35,   NA, 0.92, 0.17, 2.66)
+)
+# Q4/2015 is explicitly missing (NA)
+# Q1/2016 is implicitly missing (no row!)
+stocks
+# make implicitly missing values explicit
+stocks %>% 
+  pivot_wider(names_from = year, values_from = return) %>% 
+  pivot_longer(`2015`:`2016`, names_to = "year")
+# using complete()
+stocks %>% 
+  complete(year, qtr)
+
+# here: missing data means "take previous value forward"
+treatment <- tribble(
+  ~ person,           ~ treatment, ~response,
+  "Derrick Whitmore", 1,           7,
+  NA,                 2,           10,
+  NA,                 3,           9,
+  "Katherine Burke",  1,           4
+)
+treatment
+# this can be accomplished using fill()
+treatment %>% 
+  fill(person)
+
+
+### Case Study
+
+# WHO Tuberculosis report 2014
+who
+
+# put 
+who1 <- who %>%
+  pivot_longer(
+    new_sp_m014:newrel_f65,
+    names_to = "key",
+    values_to = "cases",
+    values_drop_na = TRUE
+  )
+who1 %>% count(key)
+
+# correct inconsistent keys (newrel instead of new_rel)
+who2 <- who1 %>%
+  mutate(key = stringr::str_replace(key, "newrel", "new_rel"))
+who2
+
+# separate if case is "new", the TB "type" 
+# from the combination of "sex" and "age"
+who3 <- who2 %>% 
+  separate(key, c("new", "type", "sexage"), sep = "_")
+who3
+
+# are all cases new? yes
+who3 %>% 
+  count(new)
+
+# let's remove "new" and redundant country information (iso2/iso3)
+who4 <- who3 %>% select(-new, -iso2, -iso3)
+who4
+
+# now separate "sex" from "age" (by position)
+who5 <- who4 %>% 
+  separate(sexage, c("sex", "age"), sep = 1)
+who5
+
+
+# the whole procedure in one step
+who %>%
+  pivot_longer(
+    cols = new_sp_m014:newrel_f65, 
+    names_to = "key", 
+    values_to = "cases", 
+    values_drop_na = TRUE
+  ) %>% 
+  mutate(
+    key = stringr::str_replace(key, "newrel", "new_rel")
+  ) %>%
+  separate(key, c("new", "var", "sexage")) %>% 
+  select(-new, -iso2, -iso3) %>% 
+  separate(sexage, c("sex", "age"), sep = 1)
