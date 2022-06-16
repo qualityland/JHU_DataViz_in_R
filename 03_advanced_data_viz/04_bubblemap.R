ed8ffbd0bf2f738dbb959a85e1a517b934153300
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2022-06-16 16:16:36 +0200

    no duplicate hashes

--- 03_advanced_data_viz/04_bubblemap.R
+++ 03_advanced_data_viz/04_bubblemap.R
@@ -1,9 +1,9 @@
-######bubble maps
+# bubble maps
 
 library(tidyverse)
 library(maps)
 
-####this provides city data
+# this provides city data
 
 world <- map_data("world")
 
@@ -11,7 +11,7 @@ japan <- filter(world,region=="Japan")
 
 ggplot(data = japan, mapping = aes(x= long, y= lat, group = group))+geom_polygon(color="black",fill="white")
 
-######maps package has city data
+# maps package has city data
 
 head(maps::world.cities)
 my_cities <-maps::world.cities
@@ -20,13 +20,13 @@ japan_cities <- filter(my_cities,country.etc=="Japan")
 
 head(japan_cities)
 
-####make a point for every city
+# make a point for every city
 ggplot(data = japan, mapping = aes(x= long, y= lat, group = group))+
   geom_polygon(color="black",fill="white")+
   geom_point(data = japan_cities,aes(x=long,y=lat,group=NULL),color="red")
 
 
-####let's pick just the big cities
+# let's pick just the big cities
 
 japan_big_cities <- filter(my_cities,country.etc=="Japan" & pop>500000)
 
@@ -35,12 +35,12 @@ ggplot(data = japan, mapping = aes(x= long, y= lat, group = group))+
   geom_point(data = japan_big_cities,aes(x=long,y=lat,group=NULL),color="red")
 
 
-####vary size of point by city size
+# vary size of point by city size
 ggplot(data = japan, mapping = aes(x= long, y= lat, group = group))+
   geom_polygon(color="black",fill="white")+
   geom_point(data = japan_big_cities,aes(x=long,y=lat,group=NULL,size=pop),color="red")
 
-#####now vary the color of the dots
+# now vary the color of the dots
 
 japan_big_cities$qual <- sample(LETTERS[1:5],nrow(japan_big_cities),replace=TRUE)
 
@@ -48,15 +48,15 @@ ggplot(data = japan, mapping = aes(x= long, y= lat, group = group))+
   geom_polygon(color="black",fill="white")+
   geom_point(data = japan_big_cities,aes(x=long,y=lat,group=NULL,color=qual,size=pop))
 
-#####do some tweaking
-####no scientific notation in legend r ggplot -- scales package adds the "scale_size_continuous" function to our arsenal, and we can set label=comma
+# do some tweaking
+# no scientific notation in legend r ggplot -- scales package adds the "scale_size_continuous" function to our arsenal, and we can set label=comma
 library(scales)
 
-###change the column name to make the legend nicer
+# change the column name to make the legend nicer
 japan_big_cities$Population <- japan_big_cities$pop
 japan_big_cities$Qualitative <- japan_big_cities$qual
 
-####do some additional refining
+# do some additional refining
 ggplot(data = japan, mapping = aes(x= long, y= lat, group = group))+geom_polygon(color="black",fill="white")+
   geom_point(data = japan_big_cities,aes(x=long,y=lat,group=NULL,color=Qualitative,size=Population))+
   scale_size_continuous(label=comma)
--- 03_advanced_data_viz/06_gganimate.R
+++ 03_advanced_data_viz/06_gganimate.R
@@ -1,29 +1,29 @@
-#install.packages("gganimate")  ---- if you need to install
-#install.packages("gifski") --- needed for PCs
+# install.packages("gganimate")  ---- if you need to install
+# install.packages("gifski") --- needed for PCs
 
 library(tidyverse)
 library(gganimate)
 library(gifski)
 library(transformr)
 
-####this example from gganimate documentation
+# this example from gganimate documentation
 
 data(mtcars)
 
 mtcars$gear
 
-###single static boxplot
+# single static boxplot
 ggplot(mtcars, aes(x=factor(cyl), y=mpg)) +
   geom_boxplot()
 
-###facetted box plot
+# facetted box plot
 ggplot(mtcars, aes(x=factor(cyl), y=mpg)) +
   geom_boxplot()+
   facet_wrap(~gear)
 
 my_anim<- ggplot(mtcars, aes(x=factor(cyl), y=mpg)) +
   geom_boxplot() +
-  # Here comes the gganimate code
+  #  Here comes the gganimate code
   transition_states(
     gear)
 
@@ -31,13 +31,13 @@ my_anim
 
 ?transition_states
 
-###Example of transition_time
+# Example of transition_time
 
 ?transition_time
 
 cel <- read_csv(url("https://www.dropbox.com/s/4ebgnkdhhxo5rac/cel_volden_wiseman%20_coursera.csv?raw=1"))
 
-###Plot total number of D and Rs across Congresses
+# Plot total number of D and Rs across Congresses
 
 cel$party<-recode(cel$dem,`1`="Democrat",`0`="Republican")
 
@@ -51,7 +51,7 @@ anim2<-ggplot(cong_dat,aes(x=year,y=Seats,fill=party))+
   scale_fill_manual(values=c("blue","red"))+
   transition_time(year)
 
-###transition_layers
+# transition_layers
 ?transition_layers
 
 ggplot()+
@@ -64,7 +64,7 @@ ggplot()+
   
 
 
-#####entering and exiting
+# entering and exiting
 
 ggplot(mtcars, aes(factor(cyl), mpg)) +
   geom_boxplot()
@@ -73,14 +73,14 @@ anim <- ggplot(mtcars, aes(factor(cyl), mpg)) +
   geom_boxplot() +
   transition_states(factor(cyl))
 
-# Fade-in, fade-out
+#  Fade-in, fade-out
 anim1 <- anim +
   enter_fade() +
   exit_fade()
 
 anim1
 
-#######shadowing
+# shadowing
 
 anim3<-ggplot(cong_dat,aes(x=year,y=Seats,fill=party))+
   geom_bar(stat="identity")+
@@ -91,7 +91,7 @@ anim3<-ggplot(cong_dat,aes(x=year,y=Seats,fill=party))+
 
 anim3
 
-####the animation will save to your working directory
+# the animation will save to your working directory
 anim_save("test.gif",animation=anim3)
 
 
