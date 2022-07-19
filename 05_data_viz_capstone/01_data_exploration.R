5c12300b6d238598767ba487293827b08b7a63b1
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2022-07-19 18:13:00 +0200

    coutry names added

---
+++ 05_data_viz_capstone/01_data_exploration.R
@@ -0,0 +1,42 @@
+library(tidyverse)
+
+mort <- read_csv("05_data_viz_capstone/data/stmf.csv", skip = 2)
+
+#countries <- read_csv("05_data_viz_capstone/data/countries_codes_and_coordinates.csv")
+countries <- 
+  read_csv(
+    "https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/slim-3/slim-3.csv",
+    col_select = c("name", "alpha-3"))
+names(countries) <- c("Country", "CountryCode")
+
+# check if we have country name for all our country codes
+mort_codes <- sort(unique(mort$CountryCode))
+all(mort_codes %in% countries$CountryCode)
+
+# which country names are missing?
+mort_codes[!(mort_codes %in% country$alpha_3)]
+# missing are:
+# "DEUTNP"  "FRATNP"  "GBR_NIR" "GBR_SCO" "GBRTENW" "NZL_NP"
+# which are:
+# "Germany" "France"  "Northern Ireland" "Scottland" "Wales & England" "New Zealand"
+
+missing_countries <- 
+  tibble(
+    Country = c("Germany", "France", "Northern Ireland", 
+                "Scottland", "Wales & England", "New Zealand"),
+    CountryCode = c("DEUTNP", "FRATNP", "GBR_NIR",
+                    "GBR_SCO", "GBRTENW", "NZL_NP")
+  )
+
+# add column: Country
+mort <- mort %>% 
+  dplyr::left_join(rbind(countries, missing_countries), by = "CountryCode")
+
+# any NAs?
+summary(mort)
+
+# show
+mort %>% 
+  select(c(Country, CountryCode)) %>% 
+  distinct()
+
