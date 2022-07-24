0f55c0b6aeeb2e0e2317033163dd17716361640d
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2022-07-24 08:38:41 +0200

    all data in one csv

--- 01_data_wrangling.R
+++ 01_data_wrangling.R
@@ -1,49 +1,103 @@
 library(tidyverse)
 
-### Countries and Country Codes ###
+#### Countries and Country Codes ####
 
-# countries with country codes
+# get country names and country codes
 countries <- 
-  read_csv(
-    "https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/slim-3/slim-3.csv",
-    col_select = c("name", "alpha-3"))
+  read_csv(file = "05_data_viz_capstone/data/slim-3.csv",
+           col_select = c("name", "alpha-3"))
+
+# change column names
 names(countries) <- c("Country", "CountryCode")
 
-# mortality data
-mort <- read_csv("05_data_viz_capstone/data/stmf.csv", skip = 2)
-
-
-# some country codes are missing
+# missing countries
 missing_countries <- 
   tibble(
     Country = c("Germany", "France", "Northern Ireland", 
                 "Scottland", "Wales & England", "New Zealand"),
     CountryCode = c("DEUTNP", "FRATNP", "GBR_NIR",
                     "GBR_SCO", "GBRTENW", "NZL_NP"))
-
 # add missing countries
 countries <- rbind(countries, missing_countries)
 
-# map countries to country codes
-mort_codes <- sort(unique(mort$CountryCode))
-country_codes <- tibble(CountryCode = mort_codes)
-country_codes <- 
-  country_codes %>% 
-  left_join(countries, by = "CountryCode")
 
-# write country codes to csv
-write_csv(country_codes, "05_data_viz_capstone/data/country_codes.csv")
+#### Mortality Data ####
+
+# get mortality data
+mort <- read_csv("05_data_viz_capstone/data/stmf.csv", skip = 2)
+
+# add column: Country
+mort <- mort %>% 
+  dplyr::left_join(countries, by = "CountryCode") %>% 
+  relocate("Country")
+
+# select required columns
+mort <- mort %>% 
+  filter(Sex == "b") %>% 
+  select(Country,
+         CountryCode,
+         Year,
+         Week,
+         D0_14, D15_64, D65_74, D75_84, D85p, DTotal)
+
+# write csv file
+write_csv(mort, "05_data_viz_capstone/data/mortality.csv")
+
+
+
+
+
+# get country names and country codes
+# countries <- 
+#   read_csv(
+#     "https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/slim-3/slim-3.csv",
+#     col_select = c("name", "alpha-3"))
+# names(countries) <- c("Country", "CountryCode")
+
+# # mortality data
+# mort <- read_csv("05_data_viz_capstone/data/stmf.csv", skip = 2)
+
+
+# # some country codes are missing
+# missing_countries <- 
+#   tibble(
+#     Country = c("Germany", "France", "Northern Ireland", 
+#                 "Scottland", "Wales & England", "New Zealand"),
+#     CountryCode = c("DEUTNP", "FRATNP", "GBR_NIR",
+#                     "GBR_SCO", "GBRTENW", "NZL_NP"))
+# 
+# # add missing countries
+# countries <- rbind(countries, missing_countries)
+
+# map countries to country codes
+# mort_codes <- sort(unique(mort$CountryCode))
+# country_codes <- tibble(CountryCode = mort_codes)
+# country_codes <- 
+#   country_codes %>% 
+#   left_join(countries, by = "CountryCode")
+
+# # write country codes to csv
+# write_csv(country_codes, "05_data_viz_capstone/data/country_codes.csv")
 
 
 
 
 ### Mortality Data ###
 
-mort <- mort %>% 
-  filter(Sex == "b") %>% 
-  select(CountryCode,
-         Year,
-         Week,
-         D0_14, D15_64, D65_74, D75_84, D85p, DTotal)
+# mort <- mort %>% 
+#   filter(Sex == "b") %>% 
+#   select(CountryCode,
+#          Year,
+#          Week,
+#          D0_14, D15_64, D65_74, D75_84, D85p, DTotal)
+# 
+# write_csv(mort, "05_data_viz_capstone/data/mortality.csv")
 
-write_csv(mort, "05_data_viz_capstone/data/mortality.csv")
+# mort <- mort %>% 
+#   filter(Sex == "b") %>% 
+#   select(CountryCode,
+#          Year,
+#          Week,
+#          D0_14, D15_64, D65_74, D75_84, D85p, DTotal)
+# 
+# write_csv(mort, "05_data_viz_capstone/data/mortality.csv")
