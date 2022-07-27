cb35bbb57acc99782d0273d8cd1a52429c1c4c8f
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2022-07-27 07:58:52 +0200

    code formating

--- 05_data_viz_capstone/02_import_code.R
+++ 05_data_viz_capstone/02_import_code.R
@@ -1,22 +1,26 @@
-#### Common import functions from the tidyverse
 library(tidyverse)
+
+
+#### Common import functions from the tidyverse
 
 read_csv("test_import.csv")
 read_csv2("test_import_semi.csv")
-read_delim("test_import_delim.txt",delim="|")
+read_delim("test_import_delim.txt", delim = "|")
 read_tsv("test_import_tab.txt")
+
 
 #### Arguments that might help (or hurt) with import
 
-#treat the first row as a header/column names or not.
-read_csv("test_import.csv",col_names=FALSE)
+# treat the first row as a header/column names or not.
+read_csv("test_import.csv", col_names = FALSE)
 
-#add header
-read_csv("test_import.csv",col_names=c("new1","new2"))
+# add header
+read_csv("test_import.csv", col_names = c("new1", "new2"))
 
-#skip some number of rows or read up to only some number of rows
+# skip some number of rows or read up to only some number of rows
 
-read_csv("test_import.csv",skip=3,n_max=2,col_names=c("Var1","Var2"))
+read_csv("test_import.csv", skip = 3, n_max = 2, col_names = c("Var1", "Var2"))
+
 
 #### Data import functions from other packages
 
