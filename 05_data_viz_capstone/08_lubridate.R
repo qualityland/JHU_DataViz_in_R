4d5bfb96da50f347cc1d5dab403b3cbe70ef2e7e
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2022-08-02 19:29:37 +0200

    lubridate cleanup

--- 05_data_viz_capstone/08_lubridate.R
+++ 05_data_viz_capstone/08_lubridate.R
@@ -1,20 +1,21 @@
-#### Lubridate Examples
+# Lubridate Examples
 
 library(tidyverse)
 #install.packages("lubridate")
 library(lubridate)
 
-#### There are three kinds of "temporal" data in the tidyverse
+# There are three kinds of "temporal" data in the tidyverse
 
 # A date
 # A time
 # A date-time, which is a time plus a date.
 
-#### The most common usage of lubridate is to parse character strings into dates
+# The most common usage of lubridate is to parse character strings into dates
 
-#Take a character string that signifies a date, like "2020-01-01" and turn it into a date
+# Take a character string that signifies a date, like "2020-01-01" and
+# turn it into a date
 
-my_dates<-c(
+my_dates <- c(
   "2020-01-01",
   "2020-01-02",
   "2020-01-03",
@@ -26,35 +27,32 @@ my_dates<-c(
   "2020-02-04"
 )
 
-my_values<-c(
-  1,2,3,4,5,6,7,8
+my_values <- c(
+  1, 2, 3, 4, 5, 6, 7, 8
 )
 
-dat<-tibble(my_dates,my_values)
+dat <- tibble(my_dates, my_values)
 
-dat$my_dates[4]-dat$my_dates[1]
+dat$my_dates[4] - dat$my_dates[1]
 
-####ymd() will parse the character string that falls into that format
+# ymd() will parse the character string that falls into that format
+dat$my_dates <- ymd(dat$my_dates)
 
-dat$my_dates<-ymd(dat$my_dates)
+# now we can do math with dates, and our plots will behave better
+dat$my_dates[4] - dat$my_dates[1]
 
-####now we can do math with dates, and our plots will behave better
+# other date parsers are mdy, myd, dmy, yq, and so forth.
 
-dat$my_dates[4]-dat$my_dates[1]
+# Once you have data as a date, you can extract components of the date
 
-####other date parsers are mdy, myd, dmy, yq, and so forth.
-
-#### Once you have data as a date, you can extract components of the date
-
-dat$month<-month(my_dates)
+dat$month <- month(my_dates)
 dat$month
 year(my_dates)
 day(my_dates)
-dat$wday<-wday(my_dates,label=TRUE)
+dat$wday <- wday(my_dates, label = TRUE)
 dat$wday
-#### As the documentation points out, putting your dates into date-time format helps with math. Resolves inconsistencies associated with leap days, February having fewer days, and the like. Be very cautious when manipulating dates mathematically.
-
-
-
-
+# As the documentation points out, putting your dates into date-time format
+# helps with math. Resolves inconsistencies associated with leap days,
+# February having fewer days, and the like. Be very cautious when manipulating
+# dates mathematically.
 
