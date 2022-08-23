39c0986f6c8d27a1567fa440d57c5dd4b9d0cb61
Stefan Schmidt <stefan.joachim.schmidt@gmail.com>
2022-08-23 12:19:27 +0200

    weekly DR by age group

--- 05_data_viz_capstone/12_final_report_script.R
+++ 05_data_viz_capstone/12_final_report_script.R
@@ -13,6 +13,14 @@ df_mort <- read_csv(url(data_file))
 
 df_mort
 
+# Years available for a specific Country
+df_mort %>% 
+  filter(Country == "Switzerland") %>% 
+  select(Year) %>% 
+  unique() %>% 
+  arrange(Year) %>% 
+  as_vector()
+
 # df %>% 
 #   group_by(Country) %>% 
 #   summarize(MinYear = min(Year),
@@ -63,6 +71,29 @@ df_mort %>%
     title="relative Mortality for different Countries",
     x="Week of the Year",
     y="Deathrate")
+
+
+
+# Age Groups
+# Absolute Deaths
+df_mort %>% 
+  filter(CountryCode == "CHE", Year == 2021) %>% 
+  select(Country, Year, Week, D0_14, D15_64, D65_74, D75_84, D85p, DTotal) %>% 
+  pivot_longer(
+    cols = starts_with("D"),
+    names_to = "Age",
+    names_prefix = "D",
+    values_to = "Deaths") %>% 
+  mutate(Age = recode(Age,
+                      `0_14` = "0 - 14 years",
+                      `15_64` = "15 - 64 years",
+                      `65_74` = "65 - 74 years",
+                      `75_84` = "75 - 84 years",
+                      `85p` = "85+ years",
+                      `Total` = "all ages")) %>% 
+  mutate(Age = factor(Age)) %>% 
+  ggplot(aes(Week, Deaths, color = Age)) +
+  geom_line()
 
 
 
